dcf57a6dfd912e6ac8d50b1f6f127de0725ba58a
Yamen Allaf <yamen_allaf@rawiplatform.com>
2024-11-13 00:19:16 +0300

    taking the dust off

--- Assets/Procedural meshes scripts/SimpleProceduralMesh.cs
+++ Assets/Procedural meshes scripts/SimpleProceduralMesh.cs
@@ -2,17 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class SimpleProceduralMesh : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void OnEnable()
     {
-        
-    }
+        var mesh = new Mesh{ name = "Procedural Mesh"};
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        mesh.vertices = new Vector3[]
+        {
+            Vector3.zero,
+            Vector3.right,
+            Vector3.up
+        };
+
+        mesh.triangles = new int[]
+        {
+            0, 2, 1
+        };
+
+        GetComponent<MeshFilter>().mesh = mesh;
     }
 }
