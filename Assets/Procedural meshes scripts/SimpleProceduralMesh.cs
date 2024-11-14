04a5f5c47ed732a9533ac3501c0411377700ff8b
Yamen Allaf <yamen_allaf@rawiplatform.com>
2024-11-14 18:14:10 +0300

    marks the end of Create a mesh tutorial

--- Assets/Procedural meshes scripts/SimpleProceduralMesh.cs
+++ Assets/Procedural meshes scripts/SimpleProceduralMesh.cs
@@ -14,12 +14,31 @@ public class SimpleProceduralMesh : MonoBehaviour
         {
             Vector3.zero,
             Vector3.right,
-            Vector3.up
+            Vector3.up,
+            new Vector3(1f, 1f)
         };
 
         mesh.triangles = new int[]
         {
-            0, 2, 1
+            0, 2, 1, 1, 2, 3
+        };
+
+        mesh.normals = new Vector3[]
+        {
+           	Vector3.back, Vector3.back, Vector3.back, Vector3.back
+        };
+
+        mesh.uv = new Vector2[]
+        {
+            Vector2.zero, Vector2.right, Vector2.up, Vector2.one
+        };
+
+        mesh.tangents = new Vector4[]
+        {
+            new Vector4(1f, 0f, 0f, -1f),
+            new Vector4(1f, 0f, 0f, -1f),
+            new Vector4(1f, 0f, 0f, -1f),
+            new Vector4(1f, 0f, 0f, -1f),
         };
 
         GetComponent<MeshFilter>().mesh = mesh;
