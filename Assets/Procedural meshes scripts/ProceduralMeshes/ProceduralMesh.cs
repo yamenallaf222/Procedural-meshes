5c36fe5a82c1c0255be750f8971aa8ccb9e01f7c
Yamen Allaf <yamen_allaf@rawiplatform.com>
2024-11-23 22:23:54 +0300

    gizmos are now drawable to show vertices and their normals and tangents

--- Assets/Procedural meshes scripts/ProceduralMeshes/ProceduralMesh.cs
+++ Assets/Procedural meshes scripts/ProceduralMeshes/ProceduralMesh.cs
@@ -3,10 +3,20 @@ using ProceduralMeshes.Generators;
 using ProceduralMeshes.Streams;
 using UnityEngine;
 using UnityEngine.Rendering;
+using UnityEngine.UIElements;
 
 [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
 public class ProceduralMesh : MonoBehaviour
 {
+	[System.Flags]
+	public enum GizmoMode {Nothing = 0, Vertices = 1, Normals = 0b10, Tangents = 0b100}
+
+
+
+	[SerializeField]
+	GizmoMode gizmos;
+	Vector3[] vertices, normals;
+	Vector4[] tangents;
 	static MeshJobScheduleDelegate[] jobs = {
 		MeshJob<SquareGrid, SingleStream>.ScheduleParallel,
 		MeshJob<SharedSquareGrid, SingleStream>.ScheduleParallel,
@@ -39,6 +49,10 @@ public class ProceduralMesh : MonoBehaviour
 	void Update(){
 		GenerateMesh();
 		enabled = false;
+
+		vertices = null;
+		normals = null;
+		tangents = null;
 	}
 
 	void GenerateMesh(){
@@ -51,5 +65,44 @@ public class ProceduralMesh : MonoBehaviour
 
 		Mesh.ApplyAndDisposeWritableMeshData(meshDataArray, mesh);
     }
+
+	void OnDrawGizmos()
+	{
+		if( gizmos == GizmoMode.Nothing || mesh == null) {
+			return;
+		}
+
+		bool drawVertices = (gizmos & GizmoMode.Vertices) != 0;
+		bool drawNormals = (gizmos & GizmoMode.Normals) != 0;
+		bool drawTangents = (gizmos & GizmoMode.Tangents) != 0;
+
+		if(vertices == null) {
+			vertices = mesh.vertices;
+		}
+		if(drawNormals && normals == null) {
+			normals = mesh.normals;
+		}
+		if(drawTangents && normals == null) {
+			tangents = mesh.tangents;
+		}
+
+		Transform t = transform;
+		for(int i = 0; i < vertices.Length; i++) {
+			Vector3 position = t.TransformPoint(vertices[i]);
+			if(drawVertices) {
+				Gizmos.color = Color.cyan;
+				Gizmos.DrawSphere(vertices[i], 0.02f);
+			}
+			if(drawNormals) {
+				Gizmos.color = Color.green;
+				Gizmos.DrawRay(position, t.TransformDirection(normals[i]) * 0.2f);
+			}
+			if(drawTangents) {
+				Gizmos.color = Color.red;
+				Gizmos.DrawRay(position, t.TransformDirection(tangents[i]) * 0.2f);
+			}
+		}
+
+	}
     
 }
