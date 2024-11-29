993cdc579e5d9e7025220b259b0fa424c51ddd57
Yamen Allaf <yamen_allaf@rawiplatform.com>
2024-11-30 00:14:07 +0300

    final seamless cube sphere complete

--- Assets/Procedural meshes scripts/ProceduralMeshes/Generators/SharedCubeSphere.cs
+++ Assets/Procedural meshes scripts/ProceduralMeshes/Generators/SharedCubeSphere.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using Unity.Mathematics;
 using UnityEngine;
 
@@ -16,46 +17,56 @@ namespace ProceduralMeshes.Generators {
 				uvOrigin = -1f,
 				uVector = 2f * right(),
 				vVector = 2f * up(),
+				seamStep = 4
 			},
 			1 => new Side {
 				id = id,
 				uvOrigin = float3(1f, -1f, -1f),
 				uVector = 2f * forward(),
 				vVector = 2f * up(),
+				seamStep = 4
 			},
 			2 => new Side {
 				id = id,
 				uvOrigin = -1f,
 				uVector = 2f * forward(),
 				vVector = 2f * right(),
+				seamStep = -2
 			},
 			3 => new Side {
 				id = id,
 				uvOrigin = float3(-1f, -1f, 1f),
 				uVector = 2f * up(),
 				vVector = 2f * right(),
+				seamStep = -2
 			},
 			4 => new Side {
 				id = id,
 				uvOrigin = -1f,
 				uVector = 2f * up(),
 				vVector = 2f * forward(),
+				seamStep = -2
+
 			},
 			_ => new Side {
 				id = id,
 				uvOrigin = float3(-1f, 1f, -1f),
 				uVector = 2f * right(),
 				vVector = 2f * forward(),
+				seamStep = -2
 			}
 		};
 
 		struct Side {
 			public int id;
 			public float3 uvOrigin, uVector, vVector;
+			public int seamStep;
+
+			public bool TouchesMinimumPole => (id & 1) == 0;
 		}
 
 		public int Resolution { get; set; }
-        public int VertexCount => 6 * 4 * Resolution * Resolution;
+        public int VertexCount => 6 * Resolution * Resolution + 2;
 
 		public int IndexCount => 6 * 6 * Resolution * Resolution;
 
@@ -70,17 +81,52 @@ namespace ProceduralMeshes.Generators {
 
 			var side = GetSide(i - 6 * u);
 
-			int vi = 4 * Resolution * (Resolution * side.id + u);
+			int vi = Resolution * (Resolution * side.id + u) + 2;
 			int ti = 2 * Resolution * (Resolution * side.id + u);
+			bool firstColumn = u == 0;
+			u += 1;
 
 			float3 pStart = side.uvOrigin + side.uVector * u / Resolution;
 			// float3 uB = side.uvOrigin + side.uVector * (u + 1) / Resolution;
 			// float3 pA = CubeToSphere(uA), pB = CubeToSphere(uB);
 
-			var vertex = new Vertex();;
-			// vertex.tangent = float4(normalize(pB - pA), -1f);
+			var vertex = new Vertex();
 
-			for(int v = 1; v <= Resolution; v++, vi += 4, ti += 2) {
+			if(i == 0) {
+				vertex.position = -sqrt(1f / 3f);
+				streams.SetVertex(0, vertex);
+				vertex.position = sqrt(1f / 3f);
+				streams.SetVertex(1, vertex);
+			}
+			// vertex.tangent = float4(normalize(pB - pA), -1f);
+			vertex.position = CubeToSphere(pStart);
+			streams.SetVertex(vi, vertex);
+
+			var triangle = int3(
+				vi,
+				firstColumn && side.TouchesMinimumPole ? 0 : vi - Resolution,
+				vi + (firstColumn ?
+					side.TouchesMinimumPole ?
+						side.seamStep * Resolution * Resolution :
+						Resolution == 1 ? side.seamStep : -Resolution + 1 :
+					-Resolution + 1
+				)
+			);
+			streams.SetTriangle(ti, triangle);
+			// streams.SetTriangle(ti + 0, 0);
+			// streams.SetTriangle(ti + 1, 0);
+			vi += 1;
+			ti +=1;
+
+
+			int zAdd = firstColumn && side.TouchesMinimumPole ? Resolution : 1;
+			int zAddLast = firstColumn && side.TouchesMinimumPole ?
+				Resolution :
+				!firstColumn && !side.TouchesMinimumPole ?
+					Resolution * ((side.seamStep + 1) * Resolution - u) + u :
+					(side.seamStep + 1) * Resolution * Resolution - Resolution + 1;
+
+			for(int v = 1; v < Resolution; v++, vi ++, ti += 2) {
 				
 				vertex.position = CubeToSphere(pStart + side.vVector * v  / Resolution);
 				streams.SetVertex(vi, vertex);
@@ -111,13 +157,37 @@ namespace ProceduralMeshes.Generators {
 				// // vertex.normal = normalize(cross(pD - pB, vertex.tangent.xyz));
 				// // vertex.texCoord0 = 1f;
 				// streams.SetVertex(vi + 3, vertex);
-				
-				streams.SetTriangle(ti + 0, 0);
-				streams.SetTriangle(ti 	+ 1, 0);
+				// triangle += 1;
+				triangle.x += 1;
+				triangle.y = triangle.z;
+				triangle.z += v == Resolution - 1 ? zAddLast : zAdd;
+				// if(v == Resolution - 1) {
+				// 	triangle.z += firstColumn && side.TouchesMinimumPole ?
+				// 		Resolution :
+				// 		!firstColumn && !side.TouchesMinimumPole ?
+				// 			Resolution * ((side.seamStep + 1) * Resolution - u) + u :
+				// 			(side.seamStep + 1) * Resolution * Resolution -
+				// 			Resolution + 1;
+				// }
+				// else {
+				// 	triangle.z += firstColumn && side.TouchesMinimumPole ? Resolution : 1;
+				// }
+
+				streams.SetTriangle(ti + 0, int3(triangle.x - 1, triangle.y, triangle.x));
+				streams.SetTriangle(ti + 1, triangle);
 
 				// pA = pC;
 				// pB = pD;
 			}
+
+			streams.SetTriangle(ti, int3(
+				triangle.x,
+				triangle.z,
+				side.TouchesMinimumPole ?
+					triangle.z + Resolution : 
+					u == Resolution ? 1 : triangle.z + 1
+
+			));
 
 		}
     }
